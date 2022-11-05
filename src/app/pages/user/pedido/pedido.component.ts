51ffe127374daeaf362bf747aac8cdbaa21b13b2
Joel-D4Ni31 <018201540d@uandina.edu.pe>
2022-11-04 19:13:56 -0500

    commit03-ProyectoFinalU2

--- src/app/pages/user/pedido/pedido.component.ts
+++ src/app/pages/user/pedido/pedido.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-pedido',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalService: BsModalService) { }
 
   ngOnInit(): void {
   }
-
+  modalRef?: BsModalRef;
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+ 
+  }
 }
